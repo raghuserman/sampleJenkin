250f58f1300f2e6f935296dd86056e313c116bc4
rajeshbala01 <rajesh.bala@outlook.com>
2020-04-19 22:52:12 +0530

    commit update to groovy file

---
+++ jenkins.groovy
@@ -0,0 +1,45 @@
+pipeline
+{
+    agent
+    {
+    node {
+        label 'windows'
+      }
+    }
+    stages
+    {
+        stage('Start the Build')
+        {
+         steps
+            {
+           echo "start the build"
+            }
+        }
+        stage('Update the Build')
+         {
+         steps
+            {
+           echo "Update the build"
+           sh"""
+                cd /"${workspace}"
+                echo "Test Content" > test.txt
+            """
+            }
+        stage('Push to Master...')
+            {
+           echo "Push the code to Master"    
+           sh"""
+                set +x
+                git config --global user.name 
+                git config --global user.email
+                set -x
+                git remote set-url origin https://github.com/santhoshvelusamy/Run-Apache.git
+                git status
+                git add .
+                git commit -m "Pushing" | true
+                git push -u origin master
+                """ 
+            }
+        }
+    }
+}
